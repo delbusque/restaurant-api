128216e9df4c2f4bf42b4ddfa09dfd2137b56d60
delbusque <del.busque@gmail.com>
2023-10-06 11:37:16 +0300

    Fix crypto decryption issue and change password validator options

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,6 +40,7 @@ const signupUser = async (req, res) => {
 
         res.status(200).json({ email, token });
     } catch (error) {
+
         res.status(400).json({ error: error.message })
     }
 }
