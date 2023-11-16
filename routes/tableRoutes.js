1b4f06139b18354f0799f1c7e90c29315a10701c
delbusque <del.busque@gmail.com>
2023-11-16 18:29:15 +0200

    Add create table controller and route

--- routes/tableRoutes.js
+++ routes/tableRoutes.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const {
     getAllTables,
-    getOneTable
+    getOneTable,
+    createTable
 } = require('../controllers/tableController.js')
 
 const router = express.Router();
 
 router.get('/', getAllTables);
 router.get('/:id', getOneTable);
+router.post('/create', createTable);
+
 
 module.exports = router;
